93b29e8f92a582a4817807af2984ef02e669f4c6
Kieren Davies <kieren@kdavi.es>
2016-07-28 15:40:25 +0200

    Create LaTeX class and Mustache template

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Generate PDF invoices with Mustache and LaTeX
 
 ## Usage
 
-    $ ./render.rb <yaml_data_file>
+    $ ./render.rb <yaml_data_files>
+
+This produces a PDF in the working directory, named after the last YAML file.
--- render.rb
+++ render.rb
@@ -7,34 +7,54 @@ require 'fileutils'
 require 'mustache'
 require 'yaml'
 
+class MustacheLatex < Mustache
+  # Escape LaTeX newlines
+  def escapeHTML str
+    str.strip.gsub("\n", '\\\\\\\\')
+  end
+end
+
 base_dir = File.dirname(File.absolute_path(__FILE__))
 template = File.read(File.join(base_dir, 'invoice.tex.mustache'))
 
 # Load data
-data_file = ARGV[0]
-data = if data_file =~ /\.ya?ml\Z/i
-  YAML.load_file(data_file)
-else
-  raise 'only YAML supported (for now)'
+data = {}
+ARGV.each do |file|
+  raise 'only YAML supported (for now)' unless file =~ /\.ya?ml\Z/i
+  data.merge!(YAML.load_file(file))
 end
 
+# Compute missing amounts
+data['items'].each do |item|
+  item['amount'] ||= item['quantity'] * item['unitprice']
+end
+data['totalamount'] ||= data['items'].map { |item| item['amount'] }.reduce(:+)
+
+# Format currency amounts
+data['items'].each do |item|
+  unless item['unitprice'].nil?
+    item['unitprice'] = format('%.2f', item['unitprice'])
+  end
+  item['amount'] = format('%.2f', item['amount'])
+end
+data['totalamount'] = format('%.2f', data['totalamount'])
+
 # Create temporary directory
-Dir.mktmpdir 'invoice' do |tmp_dir|
+Dir.mktmpdir('invoice') do |tmp_dir|
   # Render LaTeX
-  latex_file = File.join(
-    tmp_dir,
-    File.basename(data_file).sub(/\.[^.]*\Z/, '') + '.tex'
-  )
-  File.write(latex_file, Mustache.render(template, data))
+  file_base = File.basename(ARGV.last).sub(/\.[^.]*\Z/, '')
+  latex_file = File.join(tmp_dir, file_base + '.tex')
+  File.write(latex_file, MustacheLatex.render(template, data))
 
   # Render PDF
   %x(
     cd #{tmp_dir} && \
     TEXINPUTS="#{base_dir}:$TEXINPUTS" \
-    latexmk -lualatex #{latex_file}
+    latexmk -halt-on-error -lualatex #{latex_file} 1>&2
   )
+  exit $?.exitstatus unless $?.success?
 
   # Move PDF to working directory
-  pdf_file = latex_file.sub(/\.tex\Z/, '.pdf')
+  pdf_file = File.join(tmp_dir, file_base + '.pdf')
   FileUtils.mv(pdf_file, File.basename(pdf_file))
 end
